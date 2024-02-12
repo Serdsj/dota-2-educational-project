2aaa776c2a62bd48ba8df55853279a96c863daa7
Serdsj <lifannnnof@mail.ru>
2024-02-12 14:25:03 +0300

    add env variables

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      png: {
-        quality: 90,
+// Функция для создания конфигурации Vite
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${
+    env.VITE_API_URL ?? "https://dota2-proxy-hero-list.onrender.com/"
+  }`;
+
+  return defineConfig({
+    plugins: [
+      react(),
+      ViteImageOptimizer({
+        png: {
+          quality: 90,
+        },
+      }),
+    ],
+    server: {
+      cors: true,
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
-    }),
-  ],
-  server: {
-    cors: true,
-    proxy: {
-      "/api": "http://localhost:8080/",
     },
-  },
-});
+  });
+};
